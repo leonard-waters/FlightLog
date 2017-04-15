d5a2001c01da02bfdfef1158d9501139cbfd7414
Michal Lusiak <michal.lusiak@tretton37.com>
2017-04-16 01:36:11 +0200

    Getting additional airport information from external API.

--- web/views/flight_view.ex
+++ web/views/flight_view.ex
@@ -1,3 +1,26 @@
 defmodule Flightlog.FlightView do
   use Flightlog.Web, :view
+
+  def download(airport) do
+    url = "https://airportapi02560c297e8.azurewebsites.net/api/Airports?code=gEjAp7qIbRDJRJYFYxcgZ1bl==&iataCode=" <> airport
+    response = HTTPotion.get url
+    {status, fields} = JSON.decode(response.body)
+    fields
+  end
+
+  def lat(airport) do
+    fields = download(airport)
+    fields["lat@"]
+  end
+
+  def lon(airport) do
+    fields = download(airport)
+    fields["lon@"]
+  end
+
+  def location(airport) do
+    fields = download(airport)
+    fields["city@"] <> ", " <> fields["country@"]
+  end
+
 end
