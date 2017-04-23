0ffa62cba24e1d0071e2f50a56f38b6f6404230b
Michal Lusiak <michal.lusiak@tretton37.com>
2017-04-23 02:08:43 +0200

    Adding some math to the mix :)

---
+++ lib/flightlog/math.ex
@@ -0,0 +1,20 @@
+defmodule Flightlog.Math do
+    def greatCircle(a,b) do
+        lat1 = elem(a, 0)
+        lon1 = elem(a, 1)
+        lat2 = elem(b, 0)
+        lon2 = elem(b, 1)
+
+        rlat1 = :math.pi * lat1 / 180.0
+        rlat2 = :math.pi * lat2 / 180.0
+        theta = lon1 - lon2
+        rtheta = :math.pi * theta / 180.0
+
+        degDistance = :math.acos(:math.sin(rlat1) * :math.sin(rlat2) +
+                                 :math.cos(rlat1) * :math.cos(rlat2) * :math.cos(rtheta))
+
+        earthRadius = 6371.0
+
+        degDistance * earthRadius
+    end
+end
--- web/views/flight_view.ex
+++ web/views/flight_view.ex
@@ -2,25 +2,24 @@ defmodule Flightlog.FlightView do
   use Flightlog.Web, :view
 
   def download(airport) do
-    url = "https://airportapi02560c297e8.azurewebsites.net/api/Airports?code=gEjAp7qIbRDJRJYFYxcgZ1bl==&iataCode=" <> airport
+    url = "https://airportapi02560c297e8.azurewebsites.net/api/Airports?code=gEjAp7qIbRDJRJYFYxcgZ1bl1sqCPaTBPsZLVxy5MGHRNFEU79etKw==&iataCode=" <> airport
     response = HTTPotion.get url
     {status, fields} = JSON.decode(response.body)
     fields
   end
 
-  def lat(airport) do
+  def coords(airport) do
     fields = download(airport)
-    fields["lat@"]
-  end
-
-  def lon(airport) do
-    fields = download(airport)
-    fields["lon@"]
+    {fields["lat@"], fields["lon@"]}
   end
 
   def location(airport) do
     fields = download(airport)
     fields["city@"] <> ", " <> fields["country@"]
+  end
+
+  def distance(coords1, coords2) do
+    Flightlog.Math.greatCircle(coords1, coords2)
   end
 
 end
